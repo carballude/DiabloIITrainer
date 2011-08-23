df8afa33f850a425d02f2b5a05db94ca1f6bd108
Pablo Carballude <pablo@carballude.es>
2011-08-23 13:08:18 +0100

    Initial commit

---
+++ DiabloIITrainer/MainWindow.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DiabloIITrainer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btSetHealth = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkBoxAutoHealth = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btSetHealth);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.chkBoxAutoHealth);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // btSetHealth
+            // 
+            this.btSetHealth.Location = new System.Drawing.Point(37, 54);
+            this.btSetHealth.Name = "btSetHealth";
+            this.btSetHealth.Size = new System.Drawing.Size(226, 23);
+            this.btSetHealth.TabIndex = 3;
+            this.btSetHealth.Text = "Set health";
+            this.btSetHealth.UseVisualStyleBackColor = true;
+            this.btSetHealth.Click += new System.EventHandler(this.btSetHealth_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(81, 16);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Health:";
+            // 
+            // chkBoxAutoHealth
+            // 
+            this.chkBoxAutoHealth.AutoSize = true;
+            this.chkBoxAutoHealth.Location = new System.Drawing.Point(181, 17);
+            this.chkBoxAutoHealth.Name = "chkBoxAutoHealth";
+            this.chkBoxAutoHealth.Size = new System.Drawing.Size(82, 17);
+            this.chkBoxAutoHealth.TabIndex = 0;
+            this.chkBoxAutoHealth.Text = "Auto-Health";
+            this.chkBoxAutoHealth.UseVisualStyleBackColor = true;
+            this.chkBoxAutoHealth.CheckedChanged += new System.EventHandler(this.chkBoxAutoHealth_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 89);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Diablo II Trainer - Carballude";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkBoxAutoHealth;
+        private System.Windows.Forms.Button btSetHealth;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DiabloIITrainer/MainWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace DiabloIITrainer
+{
+    public partial class MainWindow : Form
+    {
+
+        #region DllImports
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, Int32 bInheritHandle, UInt32 dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern Int32 WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, uint[] lpBuffer, UInt32 nSize, IntPtr lpNumberOfBytesWritten);
+
+        #endregion
+
+        /// <summary>
+        /// Gets Diablo II process or null if it's not loaded
+        /// </summary>
+        Func<Process> GetDiablo = () => Process.GetProcessesByName("Game").SingleOrDefault();
+
+        /// <summary>
+        /// Writes a value to a specific memory address
+        /// </summary>
+        /// <param name="address">Memory address in which we want to write</param>
+        /// <param name="value">Desired value for the address</param>
+        private void WriteAddress(IntPtr address, uint value)
+        {
+            var diablo = GetDiablo(); //Look for Diablo
+            var pHandle = OpenProcess(0x1F0FFF, 0, (UInt32)diablo.Id); //Open it
+            WriteProcessMemory(pHandle, address, new uint[] { value }, 4, (IntPtr)0); //Write the new value
+            CloseHandle(pHandle); //Close
+        }
+
+        /// <summary>
+        /// Sets the health.
+        /// </summary>
+        /// <param name="value">Desired health. Be aware that X health in game is actually X*256 here.</param>
+        private void SetHealth(uint value)
+        {
+            WriteAddress((IntPtr)0x01E42D24, value);
+            WriteAddress((IntPtr)0x04C43024, value);
+            WriteAddress((IntPtr)0x053B3124, value);
+            WriteAddress((IntPtr)0x05964624, value);
+            WriteAddress((IntPtr)0x6FBCA794, value);
+            WriteAddress((IntPtr)0x6FBCA79C, value);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btSetHealth_Click(object sender, EventArgs e)
+        {
+            SetHealth((uint)numericUpDown1.Value*256);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetHealth((uint)numericUpDown1.Value * 256);
+        }
+
+        private void chkBoxAutoHealth_CheckedChanged(object sender, EventArgs e)
+        {            
+            if (timer1.Enabled) timer1.Stop();
+            else timer1.Start();
+        }
+
+    }
+
+}
